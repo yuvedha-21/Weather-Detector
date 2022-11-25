2cebb396403e346e9812f6911e0621b9a673f09d
uv_2001 <65334844+yuvedha-21@users.noreply.github.com>
2022-11-25 23:53:50 +0530

    changes

--- weather/views.py
+++ weather/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 
+
 # Create your views here.
 def index(request):
     if request.method == 'POST':
